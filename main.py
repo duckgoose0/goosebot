866e05ea359826020e9f79efad2d1a7835be8d59
duckgoose0 <86828677+duckgoose0@users.noreply.github.com>
2022-03-05 23:24:09 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import dotenv
+import hikari
+import lightbulb
+
+dotenv.load_dotenv()
+
+bot = lightbulb.BotApp(
+    os.environ["BOT_TOKEN"],
+    intents = hikari.Intents.ALL,
+    banner = None
+    )
+
+@bot.listen(hikari.StartedEvent)
+async def startbot(event):
+    print('Bot has started!')
+
+@bot.command
+@lightbulb.command('ping', 'Returns bot ping (heartbeat command).')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ping(ctx):
+    await ctx.respond(f'Pong! Latency: {bot.heartbeat_latency*1000:.2f}ms', flags=hikari.MessageFlag.EPHEMERAL)
+    print(f'Bot responds in {bot.heartbeat_latency*1000:.2f}ms.')
+
+@bot.command
+@lightbulb.command('about', 'Shows information about the bot.')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def about(ctx):
+    await ctx.respond('''I\'m goosebot! First ideated as a VALORANT store checker, I was first created on 1 March 2022 as a multipurpose Python Discord bot.
+    
+The dev: https://twitter.com/_duckgoose_
+Current Version: v0.1''')
+
+bot.load_extensions_from("./extensions/", must_exist=True)
+
+bot.run()
